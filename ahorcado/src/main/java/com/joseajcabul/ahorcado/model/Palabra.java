9f55be9c3af21e851ca7b4c5683eb183c3d2ec40
jajcabul-2021160 <jajcabul-2021160@kinal.edu.gt>
2025-09-23 22:01:16 -0600

    feat: actualisando model palabra en ahorcado

--- ahorcado/src/main/java/com/joseajcabul/ahorcado/model/Palabra.java
+++ ahorcado/src/main/java/com/joseajcabul/ahorcado/model/Palabra.java
@@ -1,18 +1,19 @@
 package com.joseajcabul.ahorcado.model;
 import jakarta.persistence.*;
 @Entity
-@Table(name = "Palabra")
+@Table(name = "Palabras")
 public class Palabra {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_Palabra") 
     private Integer idPalabra;
     @Column(name = "palabra")
     private String palabra;
-    @Column(name = "pista_uno")
+    @Column(name = "pista_1")
     private String pistaUno;
-    @Column(name = "pista_dos")
+    @Column(name = "pista_2")
     private String pistaDos;
-    @Column(name = "pista_tres")
+    @Column(name = "pista_3")
     private String pistaTres;
 
     public Integer getIdPalabra() {
